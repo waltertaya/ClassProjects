494397323fb174121acc8e8e24b4659fe9e73f8d
waltertaya <waltermittybecker5@gmail.com>
2024-02-04 09:40:22 +0300

    Flask lessons

--- FlaskPython/flaskblog.py
+++ FlaskPython/flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
@@ -25,7 +25,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="About")
 
 
 if __name__ == '__main__':
