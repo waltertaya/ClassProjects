84ed2af02c10b2bf4af280d38d9fd50d949a7d40
waltertaya <waltermittybecker5@gmail.com>
2024-02-26 23:23:24 +0300

    Importing JSON and using watch mode

---
+++ nodejsTutorial/ImportingJSON/index.js
@@ -0,0 +1,5 @@
+const data = require('./data.json')
+// const data = require('./data') // Possible but not recommended if there exist a file named data.js
+
+console.log(data.name);
+console.log(data);
