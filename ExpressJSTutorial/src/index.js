a383c6dc2dc085d813a593564330cecedc9e6201
waltertaya <waltermittybecker5@gmail.com>
2024-03-03 22:29:41 +0300

    Routes and methods

--- ExpressJSTutorial/src/index.js
+++ ExpressJSTutorial/src/index.js
@@ -1,10 +1,43 @@
 const express = require('express');
 
 const app = express();
+const PORT = 8000;
 
-const PORT = 3000;
-app.listen(PORT);
+// Register middleware
+app.use(express.json());
+app.use(express.urlencoded());
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-//     });
+let laptops = [
+    {
+        name: 'Asus',
+        number: 39
+    }
+]
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on port: ' + PORT);
+});
+
+// Middleware using next function to call the next function
+// app.get('/', (request, response, next) => {
+//     console.log('Before handling request!')
+//     next();
+// }, (request, response) => {
+//     response.send(laptops);
+// });
+
+app.get('/laptop', (request, response) => {
+    response.send(laptops);
+});
+
+app.get('/laptop/:id', (request, response) => {
+    const id = request.params.id;
+    response.send(laptops[id]);
+});
+
+app.post('/laptop', (request, response) => {
+    console.log(request.body);
+    laptops.push(request.body);
+    response.send(201);
+});
